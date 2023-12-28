e44aba03b6e717c585fcfc3fad760296711104d4
Mohit Khedkar <mohitkhedkar521@gmail.com>
2023-12-29 01:31:24 +0530

    update

--- Data Structures/Arrays/Array.cpp
+++ Data Structures/Arrays/Array.cpp
@@ -1,14 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
+int main()
+{
     int arr[10];
-    for(int i=0;i<10;i++){
-        cin>>arr[i];
-
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> arr[i];
     }
-    for(int i=0;i<10;i++){
-        cout<<arr[i]<< " ";
-        
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << " ";
     }
     return 0;
 }
